3dba217883065fdeed9db339d7f28ae01f165bc3
3DRX <kjy20030228@icloud.com>
2023-05-09 01:07:04 +0800

    add feature: getFile

--- fss_server/app/src/main/java/fss_server/controllers/GetFileController.java
+++ fss_server/app/src/main/java/fss_server/controllers/GetFileController.java
@@ -1,14 +1,19 @@
 package fss_server.controllers;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fss_server.file_access.FileManager;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @CrossOrigin
@@ -19,7 +24,48 @@ public class GetFileController {
     private FileManager fileManager;
 
     @GetMapping("/getFile")
-    public ResponseEntity getFile(String path) {
-        // TODO
+    public void getFile(@RequestBody String path, HttpServletResponse response) {
+        response.setHeader("Content-Disposition", "attachment;filename=" + getFileName(path));
+        byte[] buffer = new byte[1];
+        BufferedInputStream bis = this.fileManager.streamedFile(path);
+        if (bis == null) {
+            logger.warn("File " + path + " not found.");
+            return;
+        }
+        OutputStream os = null;
+        try {
+            os = response.getOutputStream();
+            int i = bis.read(buffer);
+            int count = 0;
+            while (i != -1) {
+                count++;
+                os.write(buffer);
+                if (count % 1000 == 0) {
+                    count = 0;
+                    os.flush();
+                }
+                i = bis.read(buffer);
+            }
+            if (count != 0) {
+                os.flush();
+            }
+        } catch (Exception e) {
+            logger.error("Error sending file " + path);
+            e.printStackTrace();
+        } finally {
+            try {
+                bis.close();
+                logger.info("File " + path + " sent.");
+            } catch (IOException e2) {
+                logger.error("Error closing file " + path);
+                e2.printStackTrace();
+            }
+        }
+        return;
+    }
+
+    private String getFileName(String path) {
+        String[] parts = path.split("/");
+        return parts[parts.length - 1];
     }
 }
--- fss_server/app/src/main/java/fss_server/file_access/FileManager.java
+++ fss_server/app/src/main/java/fss_server/file_access/FileManager.java
@@ -1,8 +1,13 @@
 package fss_server.file_access;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 
 public class FileManager {
+    private final Logger logger = LoggerFactory.getLogger(FileManager.class);
 
     private String root;
 
@@ -11,15 +16,19 @@ public class FileManager {
     }
 
     public String dir(String pwd) {
-        if (!pwd.startsWith(this.root)) {
+        String fullPwd = this.root + pwd;
+        if (!fullPwd.startsWith(this.root)) {
+            logger.warn("User tried to access file outside root directory");
             return "\tno such file or directory\n";
         }
         StringBuilder sb = new StringBuilder();
-        final File folder = new File(pwd);
+        final File folder = new File(fullPwd);
         if (!folder.exists()) {
+            logger.warn("User tried to access non-existing file");
             return "\tno such file or directory\n";
         }
         if (!folder.isDirectory()) {
+            logger.warn("User tried to access file as directory");
             return "\t" + folder.getName() + "\n";
         }
         for (final File fileEntry : folder.listFiles()) {
@@ -38,5 +47,18 @@ public class FileManager {
             }
         }
         return sb.toString();
+    }
+
+    public BufferedInputStream streamedFile(String filepath) {
+        String fullpath = this.root + filepath;
+        File file = new File(fullpath);
+        BufferedInputStream bis = null;
+        try {
+            bis = new BufferedInputStream(new FileInputStream(file));
+        } catch (Exception e) {
+            logger.error("Error while reading file " + fullpath);
+            e.printStackTrace();
+        }
+        return bis;
     }
 }
